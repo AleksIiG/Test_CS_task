c2345204b8004e7dcaf3fd7cc15de07103dfbb97
AleksIiG <kyzmin21102006@gmail.com>
2025-02-16 21:21:14 +0200

    2 commit

--- api/obj/Debug/net9.0/api.AssemblyInfo.cs
+++ api/obj/Debug/net9.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e307ee139722e2b72918d8a315155d3d9a2b01e")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
